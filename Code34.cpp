0bdb0ab3aeda86f7dbfa45f11169809316babcf0
Safal Nigam <safalnigam936@gmail.com>
2024-12-02 17:16:02 +0000

    Fifth commit

--- Code34.cpp
+++ Code34.cpp
@@ -10,12 +10,12 @@
 using namespace std;
 int main(){
     int m;
+    cout<<"ENTER THE NUMBER";
     cin>>m;
     int n=1;
     for(int i=1;i<=m;i++){
         for(int j=1;j<=i;j++){
-            cout<<n<<" ";
-            n++;
+            cout<<j<<" ";
         }
         cout<<endl;
     }
