18347e827fe9d5f62c00cb4014d52dc34c3aacaf
Safal Nigam <safalnigam936@gmail.com>
2024-12-02 18:32:23 +0000

    NINTH COMMIT

---
+++ Code38.cpp
@@ -0,0 +1,24 @@
+/*
+1 1 1 1
+  2 2 2
+    3 3 
+      4
+*/
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"ENTER THE NUMBER:";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<i;j++){ // FOR PRINTING THE SPACE
+           cout<<" ";
+        }
+            for(int k=0;k<n-i;k++){
+                cout<<i;
+            }
+            cout<<endl;
+        }
+        return 0;
+}
