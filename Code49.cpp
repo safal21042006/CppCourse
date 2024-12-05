759013b853a1d51e09b71bbc93a35d8b252de6d1
Safal Nigam <safalnigam936@gmail.com>
2024-12-05 19:10:06 +0000

    Eighteenth Commit

---
+++ Code49.cpp
@@ -0,0 +1,19 @@
+// CHAPTER 6
+// BINARY NUMBER SYSTEM
+// DECIMAL TO BINARY
+#include<iostream>
+using namespace std;
+int main(){
+    int sum=0;
+    int decnumber=50;
+    int rem;
+    int pow=1;
+    while(decnumber>0){
+        rem=decnumber%2;
+        decnumber=decnumber/2;
+        sum+=(rem*pow);
+        pow=pow*10;
+     }
+     cout<<"binary number is:"<<sum<<endl;
+    return 0;
+    }
