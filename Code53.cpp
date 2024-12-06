d8f0c6379a1483a8a317e89e4f4ccbef470b39d2
Safal Nigam <safalnigam936@gmail.com>
2024-12-06 18:12:44 +0000

    Twentytwo Commit

---
+++ Code53.cpp
@@ -0,0 +1,18 @@
+// // BITWISE LEFT<<
+// MEANS ANSWER=a*2^b
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a=10,b=2;
+//     cout<<(a << b)<<endl;
+//     return 0;
+// }
+
+// BITWISE RIGHT>> // Bitwise right gives answer a/2^b
+#include<iostream>
+using namespace std;
+int main(){
+    int a=10,b=1;
+    cout<<(a << b)<<endl;
+    return 0;
+}
