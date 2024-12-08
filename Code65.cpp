266fbb7381b253f1161272047854a3b395e09897
Safal Nigam <safalnigam936@gmail.com>
2024-12-08 12:17:38 +0000

    30 Commit

---
+++ Code65.cpp
@@ -0,0 +1,19 @@
+// FIND THE MAX VALUE
+#include<iostream>
+#include<climits>
+using namespace std;
+int main(){
+    int size=7;
+    int price[size];
+    for(int i=0;i<size;i++){
+        cin>>price[i];
+    }
+    int largest= INT_MIN;
+    for(int i=0;i<size;i++){
+        if(price[i]>largest){
+            largest=price[i];
+        }     
+    }
+    cout<<"MAXIMUM PRICE IS:"<<largest<<endl;
+    return 0;
+}
